1a304ded853947ebdd6360b5c7b44ab88bd227d9
kunal swami <kunalswami29@gmail.com>
2020-07-19 12:59:59 +0530

    added the unfavourite ,favourite functionality and the changing tabs functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,33 @@
 
 // action types
 export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_TO_FAVOURITE = 'ADD_FAVOURITE';
+export const REMOVE_FROM_FAVOURITE = 'REMOVE_FAVOURITE';
+export const SET_SHOW_FAVOURITE = 'SET_SHOW_FAVOURITE';
+
+
 // action creators
 export function addMovies(movies){
     return {
         type : ADD_MOVIES,
         movies
+    }
+}
+export function addFavourite(movie){
+    return {
+        type : ADD_TO_FAVOURITE,
+        movie
+    }
+}
+export function removeFavourite(movie){
+    return {
+        type : REMOVE_FROM_FAVOURITE,
+        movie
+    }
+}
+export function setshowFavourite(val){
+    return {
+        type : SET_SHOW_FAVOURITE,
+        val
     }
 }
